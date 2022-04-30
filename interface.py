032efba2731d2bebf9a88b00030dd78e6f41473c
vvoroby <99526918+vvoroby@users.noreply.github.com>
2022-04-30 12:08:18 +0300

    изменила интерфейс главного окна
    нужно прописать команды и соответствующие функции для каждой кнопки
    добавить кнопки сохранения и возвращения назад

--- interface.py
+++ interface.py
@@ -1,65 +1,62 @@
 from tkinter import *
 
 window = Tk()
+
 #иконка приложения
 # photo = PhotoImage(file='58431506a9a7d158c60a2227.png')
 # window.iconphoto(False, photo)
 
 window.title("Приложение :'Подсчёт калорий'")
-window.geometry('400x620')
+window.geometry('350x620')
 window['bg'] = 'lavender'
 
-# завтрак
-title=Label(window, text="Завтрак",bg="khaki",fg ="indigo", font=70, width=30)
+#здесь должен быть календарь и выбор даты
+title = Label(window, text="Сегодня", bg = "lavender", fg ="indigo", font=70, width=30)
 title.grid(column=0, row=0)
 
-btn = Button(window,text = '+', bg="pink",font=20,width=4,height = 1,
-            relief=RAISED,
-            bd=6)
-btn.grid(column=1, row=0)
+title=Label(window, bg = "lavender", font=70, width=30)
+title.grid(column=0, row=1)
 
-# обед
-title=Label(window, text="Обед",bg="wheat",fg ="indigo",font=70,width=30 )
-title.grid(column=0, row=2)
-btn = Button(window,text = '+', bg="pink",font=20,width=4,height = 1,
-            relief=RAISED,
-            bd=6)
-btn.grid(column=1, row=2)
+# кнопка выбора завтрака
+btn = Button(window,text = 'Завтрак', bg="Linen", fg = "SandyBrown", font=90, width=30, height = 1, relief=RAISED, bd=6, command = breakfest)
+btn.grid(column=0, row=2)
 
-# ужин
-title=Label(window, text="Ужин",bg="cornsilk",fg ="indigo",font=70,width=30 )
-title.grid(column=0, row=4)
-btn = Button(window,text = '+', bg="pink",font=20,width=4,height = 1,
-            relief=RAISED,
-            bd=6)
-btn.grid(column=1, row=4)
+# кнопка выбоа обеда
+btn = Button(window,text="Обед", bg="Honeydew", fg ="DarkSeaGreen",font=70,width=30,height = 1,relief=RAISED,bd=6)
+btn.grid(column=0, row=3)
 
-# перекус
-title=Label(window, text="Перекус",bg="whitesmoke",fg ="indigo",font=70,width=30 )
+# кнопка выбора ужина
+btn = Button(window,text="Ужин", bg="AliceBlue",fg ="CornflowerBlue",font=70,width=30,height = 1,relief=RAISED,bd=6)
+btn.grid(column=0, row=4)
+
+# кнопка выбора перекуса
+btn = Button(window,text="Перекус",bg="MistyRose",fg ="PaleVioletRed",font=70,width=30,height = 1,relief=RAISED,bd=6)
+btn.grid(column=0, row=5)
+
+title=Label(window, bg = "lavender",font=70,width=30 )
 title.grid(column=0, row=6)
-btn = Button(window,text = '+', bg="pink",font=20,width=4,height = 1,
-            relief=RAISED,
-            bd=6)
-btn.grid(column=1, row=6)
 
-# вода
-title=Label(window, text="Вода",bg="lightcyan",fg ="indigo",font=70,width=30 )
-title.grid(column=0, row=8)
-btn = Button(window,text = '+', bg="pink",font=20,width=4,height = 1,
-            relief=RAISED,
-            bd=6)
-btn.grid(column=1, row=8)
+# # вода
+# title=Label(window, text="Вода",bg="lightcyan",fg ="indigo",font=70,width=30 )
+# title.grid(column=0, row=8)
+# btn = Button(window,text = '+', bg="pink",font=20,width=4,height = 1,
+#             relief=RAISED,
+#             bd=6)
+# btn.grid(column=1, row=8)
 
 # сводка (добавить виджеты)
-title=Label(window, text="СВОДКА",bg="thistle",fg ="indigo",font=70,width=30,
+title=Label(window, text="СВОДКА",bg="white",fg ="indigo",font=70,width=30,
             height=10,anchor = 'n')
-title.grid(column=0, row=10)
+title.grid(column=0, row=7)
 
-# кнопка выхода
-btn = Button(window, text="Закрыть окно", bg="red",font=20,width=12,height = 1,
+title=Label(window, bg = "lavender",font=70,width=30 )
+title.grid(column=0, row=8)
+
+# кнопка СОХРАНЕНИЯ и выхода
+btn = Button(window, text="Закрыть окно", bg="pink",
             relief=RAISED,
             bd = 6,
             command=window.quit)
-btn.grid()
+btn.grid(column=0, row=9)
 
 window.mainloop()
