5a5ea23e83e9c64b790328ad296cc175a171b0fb
vvoroby <99526918+vvoroby@users.noreply.github.com>
2022-05-23 02:24:04 +0300

    Update archive.py

--- main_button/archive.py
+++ main_button/archive.py
@@ -5,70 +5,83 @@ from tkinter import ttk
 from tkinter.ttk import Radiobutton
 from tkinter.ttk import Combobox
 
-# Таблица при нажатии на кнопку архив
-window = Tk()
-window.title("Завтрак")
-window.geometry('350x620')
-window['bg'] = 'Lavender'
+def archive():
+    def find_product_from_day():
+        my_frame.delete(*my_frame.get_children()) ##очищает таблицу
 
-title = Label(window, text="Архив", bg="Lavender", fg="purple4", font=70, width=30)
-title.pack(pady=5)
+        connect = sqlite3.connect('archive.db')  ##делаем запрос к базе данных
+        cursor = connect.cursor()
+        cursor.execute('''SELECT meal,name,g,kkal,p,f,c  FROM archive WHERE date = ?''', [my_date.get()])
+        all_products = cursor.fetchall()
+        connect.close()
+        print(all_products)
+        for item in all_products:
+            my_frame.insert(parent='', index='end', values=item)
+            my_frame.pack()
 
-my_date = StringVar()
+    window = Tk()
+    window.title("Archive")
+    window.geometry('350x620')
+    window['bg'] = 'Lavender'
 
-frame = Frame(window)
-date_label = Label(frame, text="Дата ", bg="lavender")
-date_entry = Entry(frame, width=8, textvariable=my_date)
-date_label.pack(side=LEFT)
-date_entry.pack(side=RIGHT)
-frame.pack(pady=5)
+    title = Label(window, text="Archive", bg="Lavender", fg="purple4", font=70, width=30)
+    title.pack(pady=5)
 
-message_button = Button(window, text="Узнать", bg="pink"##, command=calculeter)
-                        )
-message_button.pack(pady=15)
+    my_date = StringVar()
 
-#Таьлица
-main_frame = Frame(window)
-main_frame.pack(pady=40)
+    frame = Frame(window)
+    date_label = Label(frame, text="Дата ", bg="lavender")
+    date_entry = Entry(frame, width=10, textvariable=my_date)
+    date_label.pack(side=LEFT)
+    date_entry.pack(side=RIGHT)
+    frame.pack(pady=5)
 
-# бегунок
-frame_scroll = Scrollbar(main_frame)
-frame_scroll.pack(side=RIGHT, fill=Y)
+    message_button = Button(window, text="Узнать", bg="pink", command=find_product_from_day)
+    message_button.pack(pady=15)
 
-frame_scroll = Scrollbar(main_frame, orient='horizontal')
-frame_scroll.pack(side=BOTTOM, fill=X)
+    # Таьлица
+    main_frame = Frame(window)
+    main_frame.pack(pady=40)
 
-my_frame = ttk.Treeview(main_frame, yscrollcommand=frame_scroll.set, xscrollcommand=frame_scroll.set)
-my_frame.pack()
+    # бегунок
+    frame_scroll = Scrollbar(main_frame)
+    frame_scroll.pack(side=RIGHT, fill=Y)
 
-frame_scroll.config(command=my_frame.yview)
-frame_scroll.config(command=my_frame.xview)
+    frame_scroll = Scrollbar(main_frame, orient='horizontal')
+    frame_scroll.pack(side=BOTTOM, fill=X)
 
-# определение колонок
-my_frame['columns'] = ('player_eating', 'player_product', 'player_mass', 'player_kkal', 'player_p', 'player_f', 'player_c')
+    my_frame = ttk.Treeview(main_frame, yscrollcommand=frame_scroll.set, xscrollcommand=frame_scroll.set)
+    my_frame.pack()
 
-# формат колонок
-my_frame.column("#0", width=0, stretch=NO)
-my_frame.column("player_eating", anchor=CENTER, width=80)
-my_frame.column("player_product", anchor=CENTER, width=150)
-my_frame.column("player_mass", anchor=CENTER, width=80)
-my_frame.column("player_kkal", anchor=CENTER, width=80)
-my_frame.column("player_p", anchor=CENTER, width=80)
-my_frame.column("player_f", anchor=CENTER, width=80)
-my_frame.column("player_c", anchor=CENTER, width=80)
+    frame_scroll.config(command=my_frame.yview)
+    frame_scroll.config(command=my_frame.xview)
 
-# заголовки колонок
-my_frame.heading("#0", text="", anchor=CENTER)
-my_frame.heading("player_eating", text="Прием пищи", anchor=CENTER)
-my_frame.heading("player_product", text="Продукт", anchor=CENTER)
-my_frame.heading("player_mass", text="Масса", anchor=CENTER)
-my_frame.heading("player_kkal", text="Ккал", anchor=CENTER)
-my_frame.heading("player_p", text="Белки", anchor=CENTER)
-my_frame.heading("player_f", text="Жиры", anchor=CENTER)
-my_frame.heading("player_c", text="Углеводы", anchor=CENTER)
+    # определение колонок
+    my_frame['columns'] = (
+    'player_eating', 'player_product', 'player_mass', 'player_kkal', 'player_p', 'player_f', 'player_c')
 
-# кнопка СОХРАНЕНИЯ и выхода
-btn = Button(window, text="Закрыть", bg="pink", relief=RAISED, bd=1, command=window.destroy)
-btn.pack(side=TOP, pady=60)
+    # формат колонок
+    my_frame.column("#0", width=0, stretch=NO)
+    my_frame.column("player_eating", anchor=CENTER, width=80)
+    my_frame.column("player_product", anchor=CENTER, width=150)
+    my_frame.column("player_mass", anchor=CENTER, width=80)
+    my_frame.column("player_kkal", anchor=CENTER, width=80)
+    my_frame.column("player_p", anchor=CENTER, width=80)
+    my_frame.column("player_f", anchor=CENTER, width=80)
+    my_frame.column("player_c", anchor=CENTER, width=80)
 
-window.mainloop()
+    # заголовки колонок
+    my_frame.heading("#0", text="", anchor=CENTER)
+    my_frame.heading("player_eating", text="Прием пищи", anchor=CENTER)
+    my_frame.heading("player_product", text="Продукт", anchor=CENTER)
+    my_frame.heading("player_mass", text="Масса", anchor=CENTER)
+    my_frame.heading("player_kkal", text="Ккал", anchor=CENTER)
+    my_frame.heading("player_p", text="Белки", anchor=CENTER)
+    my_frame.heading("player_f", text="Жиры", anchor=CENTER)
+    my_frame.heading("player_c", text="Углеводы", anchor=CENTER)
+
+    # кнопка СОХРАНЕНИЯ и выхода
+    btn = Button(window, text="Закрыть", bg="pink", relief=RAISED, bd=1, command=window.destroy)
+    btn.pack(side=TOP, pady=60)
+
+    window.mainloop()
